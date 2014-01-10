2ae6e454abe6e33564ac269abff43e793d6e6dbf
Robin <robinlee_6316@msn.com>
2014-01-10 13:01:23 +0800

    範例:擷取網頁中的檔案名稱，進行批次下載

---
+++ DownloadFiles/GrepLoadDoc/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace GrepLoadDoc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.txtDept = new System.Windows.Forms.TextBox();
+            this.butDownload = new System.Windows.Forms.Button();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(27, 46);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(486, 22);
+            this.txtUrl.TabIndex = 0;
+            this.txtUrl.Text = "http://www.ccafps.khc.edu.tw/releaseRedirect.do?unitID=183&pageID=3357";
+            // 
+            // txtDept
+            // 
+            this.txtDept.Location = new System.Drawing.Point(27, 91);
+            this.txtDept.Name = "txtDept";
+            this.txtDept.Size = new System.Drawing.Size(97, 22);
+            this.txtDept.TabIndex = 1;
+            this.txtDept.Text = "教務處";
+            // 
+            // butDownload
+            // 
+            this.butDownload.Location = new System.Drawing.Point(27, 176);
+            this.butDownload.Name = "butDownload";
+            this.butDownload.Size = new System.Drawing.Size(97, 30);
+            this.butDownload.TabIndex = 2;
+            this.butDownload.Text = "Download";
+            this.butDownload.UseVisualStyleBackColor = true;
+            this.butDownload.Click += new System.EventHandler(this.butDownload_Click);
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(27, 135);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.ReadOnly = true;
+            this.txtFolder.Size = new System.Drawing.Size(486, 22);
+            this.txtFolder.TabIndex = 3;
+            this.txtFolder.Text = "R:\\New folder";
+            this.txtFolder.Click += new System.EventHandler(this.txtFolder_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(27, 236);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(31, 12);
+            this.lblMessage.TabIndex = 4;
+            this.lblMessage.Text = "ready";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 333);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.butDownload);
+            this.Controls.Add(this.txtDept);
+            this.Controls.Add(this.txtUrl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.TextBox txtDept;
+        private System.Windows.Forms.Button butDownload;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
+
---
+++ DownloadFiles/GrepLoadDoc/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace GrepLoadDoc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtFolder_Click(object sender, EventArgs e)
+        {
+            if (this.folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                this.txtFolder.Text = this.folderBrowserDialog1.SelectedPath;
+        }
+
+        private void butDownload_Click(object sender, EventArgs e)
+        {
+            GrepHtmlthenDownloadDoc(this.txtUrl.Text, this.txtFolder.Text, this.txtDept.Text);
+            lblMessage.Text = String.Format("done at {0}", DateTime.Now);
+        }
+
+        private void GrepHtmlthenDownloadDoc(string url, string rootFolder, string newFolder)
+        {
+            string htmlText = Spider‎WebPage(url);
+
+
+
+
+            List<string> list = ParseHtml(htmlText);
+
+
+            string saveFolder = Path.Combine(rootFolder, newFolder);
+            if (!Directory.Exists(saveFolder))
+                Directory.CreateDirectory(saveFolder);
+
+            foreach (string id in list)
+            {
+                //Download("http://www.ccafps.khc.edu.tw/fileManager.do?actionType=downloadF&file.id=197", saveFolder, newFolder);
+                Download( "http://www.ccafps.khc.edu.tw/fileManager.do?actionType=downloadF&file.id="+id
+                    , saveFolder
+                    , newFolder);
+            }
+
+        }
+
+
+
+        /// <summary>
+        /// 讀取原始的HTML
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        private string Spider‎WebPage(string url)
+        {
+            string htmlText = null;
+
+            WebRequest myWebRequest = WebRequest.Create(url);
+            myWebRequest.Timeout = 10000;
+
+            WebResponse myWebResponse = myWebRequest.GetResponse();
+            using (Stream myStream = myWebResponse.GetResponseStream())
+            {
+                using (StreamReader myStreamReader = new StreamReader(myStream))
+                {
+                    htmlText = myStreamReader.ReadToEnd();
+                }
+            }
+            return htmlText;
+        }
+
+
+        private string GetOriginalFileName(string originalFileName)
+        {
+            string fileName = System.Web.HttpUtility.UrlDecode(originalFileName);
+            string findKey="filename=";
+
+            int idx = originalFileName.IndexOf(findKey)+findKey.Length;
+            fileName = fileName.Substring(idx, fileName.Length - idx);
+
+            return fileName;
+        }
+
+
+        private void Download(string url, string saveFolder, string prefix)
+        {
+            HttpWebRequest httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+
+            string originalFileName = GetOriginalFileName(httpResponse.Headers["Content-Disposition"]);
+            
+
+            System.IO.Stream dataStream = httpResponse.GetResponseStream();
+            byte[] buffer = new byte[8192];
+
+            string newFileName = Path.Combine(saveFolder, string.Format("{0}-{1}", prefix, originalFileName));
+
+            FileStream fs = new FileStream(newFileName,
+                FileMode.Create, FileAccess.Write);
+            int size = 0;
+            do
+            {
+                size = dataStream.Read(buffer, 0, buffer.Length);
+                if (size > 0)
+                    fs.Write(buffer, 0, size);
+            } while (size > 0);
+            fs.Close();
+
+            httpResponse.Close();
+
+            Console.WriteLine("Done at " + DateTime.Now.ToString("HH:mm:ss.fff"));
+        }
+
+
+        private List<string> ParseHtml(string htmlText)
+        {
+            string[] tables = System.Text.RegularExpressions.Regex.Split(htmlText
+                , "排版用表格"
+            );
+
+            List<string> returns = new List<string>();
+            foreach (string table in tables)
+            {
+                string[] list = System.Text.RegularExpressions.Regex.Split(table
+                , "actionType=downloadF&file.id="
+                );
+
+                foreach (string html in list)
+                {
+                    //"179\" class=\"a4\" title=\"校友申請各項證明申請表.doc(開啟新視窗)\">校友申請各項證明申請表.doc</a> 
+                    string[] strs = html.Replace("\""," ").Split(new char[1] { ' ' });
+                    int id;
+                    if (Int32.TryParse(strs[0], out id))
+                        returns.Add(strs[0]);
+                }
+            }
+            
+            return returns;
+        }
+    }
+}
